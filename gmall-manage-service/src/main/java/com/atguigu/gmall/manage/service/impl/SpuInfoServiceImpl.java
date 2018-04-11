1983a8a2320f2bfbda91782e892dd8a50c829c26
wanghai <451299586@qq.com>
2018-04-11 17:49:14 +0800

    商品模块

--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SpuInfoServiceImpl.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SpuInfoServiceImpl.java
@@ -2,7 +2,7 @@ package com.atguigu.gmall.manage.service.impl;
 
 import com.alibaba.dubbo.config.annotation.Service;
 import com.atguigu.gmall.bean.SpuInfo;
-import com.atguigu.gmall.manage.mapper.SpuInfoMapper;
+import com.atguigu.gmall.manage.mapper.*;
 import com.atguigu.gmall.service.SpuInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -11,6 +11,14 @@ import java.util.List;
 public class SpuInfoServiceImpl implements SpuInfoService{
     @Autowired
     SpuInfoMapper spuInfoMapper;
+    @Autowired
+    SpuImageMapper spuImageMapper;
+    @Autowired
+    BaseSaleAttrMapper baseSaleAttrMapper;
+    @Autowired
+    SpuSaleAttrValueMapper spuSaleAttrValueMapper;
+    @Autowired
+    SpuSaleAttrMapper spuSaleAttrMapper;
     @Override
     public List<SpuInfo> getSpuInfoList(SpuInfo spuInfo) {
         //获取SpuInfo信息集合
