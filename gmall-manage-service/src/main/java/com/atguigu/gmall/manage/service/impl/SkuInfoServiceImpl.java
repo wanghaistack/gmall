d0c22461b7017a8cad68e4a964780ed67e49571b
wanghai <451299586@qq.com>
2018-04-16 20:36:54 +0800

    商品模块

--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuInfoServiceImpl.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuInfoServiceImpl.java
@@ -52,13 +52,14 @@ public class SkuInfoServiceImpl implements SkuInfoService {
     }
     @Override
     public SkuInfo getSkuInfo(String skuId) {
+
         try {
             //自定义从redis工具类中获取jedis对象
             Jedis jedis = redisUtil.getJedis();
             //拼接字符串创建Redis里面的Key值
             String skuInfoKey= JedisConst.SKU_PREFIX+skuId+JedisConst.SKU_SUFFIX;
             //根据key值获取value值
-            String skuInfoJson = jedis.get("skuInfoKey");
+            String skuInfoJson = jedis.get(skuInfoKey);
             //如果返回为空，则调用本地数据库连接
             if (skuInfoJson==null || skuInfoJson.length()==0){
                 System.out.println(Thread.currentThread().getName()+"当前缓存中未找到数据");
@@ -71,7 +72,6 @@ public class SkuInfoServiceImpl implements SkuInfoService {
                    //如果数据库里面没有值，别人恶意攻击的话，直接设置到redis缓存中
                    if (skuInfo==null){
                        jedis.setex(skuInfoKey,JedisConst.TIME_OUT,"empty");
-                       jedis.close();
                        return null;
                    }
                      skuInfoJson = JSON.toJSONString(skuInfo);
@@ -81,13 +81,13 @@ public class SkuInfoServiceImpl implements SkuInfoService {
 
 
                 } else {
-                    //等待1秒钟
-                    Thread.sleep(1*1000);
+
                     //假设之后有人取锁后dang掉了，递归调用自己去寻找钥匙。
                     System.out.println(Thread.currentThread().getName()+"未获得分布式锁，开启自旋模式俗称递归.");
-                    SkuInfo skuInfo = getSkuInfo(skuId);
+                    //等待1秒钟
+                    Thread.sleep(1*1000);
                     jedis.close();
-                    return skuInfo;
+                    return getSkuInfo(skuId);
                 }
 
             }else if (skuInfoJson.equals("empty")){
@@ -112,6 +112,13 @@ public class SkuInfoServiceImpl implements SkuInfoService {
 
     @Override
     public SkuInfo getSkuInfoDB(String skuId) {
+        System.out.println(Thread.currentThread().getName()+"查询数据库");
+        //睡3秒钟
+        try {
+            Thread.sleep(3*1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         SkuInfo skuInfo = skuInfoMapper.selectByPrimaryKey(skuId);
         if (skuInfo==null){
             return null;
