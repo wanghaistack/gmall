804b7abd0bc40fd32b1ac804fbf7d9bc2ba63094
wanghai <451299586@qq.com>
2018-04-18 18:44:10 +0800

    商品模块

--- gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuInfoServiceImpl.java
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/SkuInfoServiceImpl.java
@@ -105,6 +105,7 @@ public class SkuInfoServiceImpl implements SkuInfoService {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
         return getSkuInfoDB(skuId);
 
 
